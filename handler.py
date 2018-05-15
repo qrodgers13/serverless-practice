dafb51d095b4b81b07eabd98e4dd2b8fffd3a50d
Quinn Rodgers <qrodgers13@hotmail.com>
2018-05-14 19:24:05 -0500

    working lambda

--- handler.py
+++ handler.py
@@ -1,21 +1,17 @@
 import json
 import requests
 from twilio.rest import Client
+from config import TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, PHONE_NUMBER
+
+client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
 def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
-
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
-
-    r = requests.get('https://api.github.com/events')
-
-    return r.json()
+    message = client.messages.create(
+                                body="Hello there!",
+                                from_="+15005550006",
+                                to=PHONE_NUMBER
+                            )
+    return "sent the message..."
 
     # Use this code if you don't use the http event with the LAMBDA-PROXY
     # integration
